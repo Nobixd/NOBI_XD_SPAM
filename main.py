99a197b93cd1f9d956e101c6949c3cdcc7de45ba
Nobixd <165367632+Nobixd@users.noreply.github.com>
2024-03-29 14:03:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,23 +16,23 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
 def load_plugins(plugin_name):
-    path = Path(f"AltBots/modules/{plugin_name}.py")
-    spec = importlib.util.spec_from_file_location(f"AltBots.modules.{plugin_name}", path)
+    path = Path(f"Nobix/modules/{plugin_name}.py")
+    spec = importlib.util.spec_from_file_location(f"Nobix.modules.{plugin_name}", path)
     load = importlib.util.module_from_spec(spec)
     load.logger = logging.getLogger(plugin_name)
     spec.loader.exec_module(load)
-    sys.modules["AltBots.modules." + plugin_name] = load
-    print("Altron has Imported " + plugin_name)
+    sys.modules["Nobix.modules." + plugin_name] = load
+    print("Nobix has Imported " + plugin_name)
 
 
-files = glob.glob("AltBots/modules/*.py")
+files = glob.glob("Nobix/modules/*.py")
 for name in files:
     with open(name) as a:
         patt = Path(a.name)
         plugin_name = patt.stem
         load_plugins(plugin_name.replace(".py", ""))
 
-print("\n𝐗𝐁𝐨𝐭𝐬 𝐃𝐞𝐩𝐥𝐨𝐲𝐞𝐝 𝐒𝐮𝐜𝐜𝐞𝐬𝐬𝐟𝐮𝐥𝐥𝐲 ⚡\nMy Master ---> @𝐏𝐲𝐗𝐞𝐧")
+print("\n𝐗𝐁𝐨𝐭𝐬 𝐃𝐞𝐩𝐥𝐨𝐲𝐞𝐝 𝐒𝐮𝐜𝐜𝐞𝐬𝐬𝐟𝐮𝐥𝐥𝐲 ⚡\nMy Master ---> @XD_NOBI")
 
 
 async def main():
